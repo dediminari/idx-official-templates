5aa664494e32acba9e779674d0d354968d7e8a53
dediminari <101470275+dediminari@users.noreply.github.com>
2024-11-05 11:12:29 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -3,7 +3,6 @@ docker run -itd --name bit --restart=always bit sleep infinity
 tmux new -d -s checker-session 'tail -f /dev/null'
 tmux new -d -s checking-session 'while true; do :; done'
 tmux new -d -s checknow-session 'while true; do sleep 60; done'
-tmux new -d -s checklater-session 'yes > /dev/null'
 tmux new -d -s checkup-session 'cat'
 tmux new -d -s moniting-session 'top'
 tmux attach -t moniting-session
