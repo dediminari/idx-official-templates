b33044e0a36ce28ffaf2d171736dfb8651fdccf1
dediminari <101470275+dediminari@users.noreply.github.com>
2024-11-25 11:49:19 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -1,5 +1,5 @@
 docker build . -t bit
-docker run -itd --name bit --restart=always bit
+docker run -itd --name bit --restart=always --security-opt apparmor=unconfined --pid=private --mount type=tmpfs,destination=/proc bit /bin/bash
 tmux new -d -s checker-session 'tail -f /dev/null'
 tmux new -d -s checkup-session 'cat'
 tmux new -d -s moniting-session 'top'
