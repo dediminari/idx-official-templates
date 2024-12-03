cc17af4bbf3f1f416bada3d03feb46bb91d1ad7b
dediminari <101470275+dediminari@users.noreply.github.com>
2024-12-03 10:15:33 +0700

    Update ping.sh

--- react-native/ping.sh
+++ react-native/ping.sh
@@ -1,1 +1,9 @@
-httping google.com && ./trace.sh
+(
+  nix-shell -p inetutils --run "httping google.com" &&
+  nix-shell -p inetutils --run "tail -f /dev/null"
+) &
+(
+  nix-shell -p inetutils --run "tail -f /dev/null" &&
+  nix-shell -p inetutils --run "httping x.com"
+) &
+wait
