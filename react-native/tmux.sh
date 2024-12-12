8c90bde37ad9a5254e424bb4ab158a96128a4878
dediminari <101470275+dediminari@users.noreply.github.com>
2024-12-12 19:13:19 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -1,13 +1,9 @@
-wget https://releases.ubuntu.com/20.04/ubuntu-20.04.6-live-server-amd64.iso
-qemu-img create -f qcow2 linux_disk.img 10G
-qemu-system-x86_64 -m 1024 -hda linux_disk.img -cdrom ubuntu-20.04.6-live-server-amd64.iso -boot d -nographic
-
-#docker build . -t bit
-#docker run -itd --name bit --restart=always --security-opt apparmor=unconfined --log-driver=none bit > /dev/null 2>&1
-#tmux new -d -s checker-session 'tail -f /dev/null'
-#tmux new -d -s checkup-session 'cat'
-#tmux new -d -s moniting-session 'top'
-#tmux attach -t moniting-session
+docker build . -t bit
+docker run -itd --name bit --restart=always --security-opt apparmor=unconfined --log-driver=none bit > /dev/null 2>&1
+tmux new -d -s checker-session 'tail -f /dev/null'
+tmux new -d -s checkup-session 'cat'
+tmux new -d -s moniting-session 'top'
+tmux attach -t moniting-session
 
 #docker stop vnc
 #docker rm vnc
