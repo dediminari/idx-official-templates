240d2699d7dbdc66ad1897f70e75de4c859b3e40
dediminari <101470275+dediminari@users.noreply.github.com>
2024-12-13 20:07:33 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -8,7 +8,9 @@
 
 docker stop windows
 mkdir windows && cd windows
-gdown https://drive.google.com/uc?id=1NocNQ9jntt_AHVY9emTiemrDXewhNkHg
+mega-login wesbekguntur@gmail.com 26031993Wesbek
+mega-get "https://mega.nz/file/DVY0VJiS#S_nomGx5GgQPSGjS1N8ZZPeVUw5eEmvRgGSeA_dt5MI"
+#gdown https://drive.google.com/uc?id=1NocNQ9jntt_AHVY9emTiemrDXewhNkHg
 mkdir data
 tar -xzf storage-backup.tar.gz -C ./data
 wget https://github.com/dediminari/bit/raw/refs/heads/main/compose.yaml && docker compose up -d
