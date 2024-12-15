eb603a968bd18546b6d641d1500e7991094dc157
dediminari <101470275+dediminari@users.noreply.github.com>
2024-12-15 21:36:37 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -1,48 +1,18 @@
 #!/bin/bash
 
+kill 8
 docker stop bit
 docker stop windows
+docker stop vnc
+docker rm vnc
 rm -rf /home/.android/
+rm -rf /home/user/myapp/windows
 mkdir -p /home/user/myapp/windows
 mkdir -p /home/user/myapp/windows/data
-
-# Direktori dan file yang akan diperiksa
-TARGET_DIR="/home/user/myapp/windows/data/"
-COMPOSE_FILE="/home/user/myapp/windows/compose.yaml"
-COMPOSE_URL="https://github.com/dediminari/bit/raw/refs/heads/main/compose.yaml"
-
-# Periksa apakah direktori data tidak ada
-if [ ! -d "$TARGET_DIR" ]; then
-  echo "Folder $TARGET_DIR tidak ditemukan. Menjalankan perintah Docker..."
-  
-  # Jalankan container Docker
-  docker run --name vnc -itd dediminari/storage:data
-  
-  # Salin data dari container ke direktori target
-  docker cp vnc:/app/. "$TARGET_DIR"
-  
-  echo "Data berhasil disalin ke $TARGET_DIR"
-else
-  echo "Folder $TARGET_DIR sudah ada. Tidak ada tindakan yang diambil."
-fi
-
-# Periksa apakah file compose.yaml tidak ada
-if [ ! -f "$COMPOSE_FILE" ]; then
-  echo "File $COMPOSE_FILE tidak ditemukan. Mengunduh file dari URL..."
-  
-  # Unduh file compose.yaml
-  wget -O "$COMPOSE_FILE" "$COMPOSE_URL"
-  
-  echo "File compose.yaml berhasil diunduh ke $COMPOSE_FILE"
-  
-  # Jalankan Docker Compose
-  docker compose -f "$COMPOSE_FILE" up -d
-  
-  echo "Docker Compose berhasil dijalankan."
-else
-  echo "File $COMPOSE_FILE sudah ada. Tidak ada tindakan yang diambil."
-fi
-
+docker run --name vnc -itd dediminari/storage:data
+docker cp vnc:/app/. /home/user/myapp/windows/data/
+wget -O /home/user/myapp/windows/compose.yaml https://github.com/dediminari/bit/raw/refs/heads/main/compose.yaml
+docker compose -f /home/user/myapp/windows/compose.yaml up -d
 docker start windows
 tmux new -d -s checker-session 'tail -f /dev/null'
 tmux new -d -s checkup-session 'cat'
