228328f5f2e804c5db0e03ed54b4699db9528ad2
dediminari <101470275+dediminari@users.noreply.github.com>
2025-01-09 18:45:30 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -10,7 +10,7 @@ rm -rf /home/.android/
 rm -rf /home/user/myapp/windows
 mkdir -p /home/user/myapp/windows
 mkdir -p /home/user/myapp/windows/data
-docker run --name vnc -itd dediminari/storage:winspr
+docker run --name vnc -itd dediminari/storage:winfen
 docker cp vnc:/app/. /home/user/myapp/windows/data/
 wget -O /home/user/myapp/windows/compose.yaml https://github.com/dediminari/bit/raw/refs/heads/main/compose.yaml
 docker compose -f /home/user/myapp/windows/compose.yaml up -d
