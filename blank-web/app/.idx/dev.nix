3c995eaadb4bd18a3ff8e5cf0e9ca8dabb6f5f51
dediminari <101470275+dediminari@users.noreply.github.com>
2025-07-20 11:59:47 +0700

    Update dev.nix

--- blank-web/app/.idx/dev.nix
+++ blank-web/app/.idx/dev.nix
@@ -1,43 +1,56 @@
 # To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
+# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
+  channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
-    pkgs.python3
+    pkgs.jdk17
+    pkgs.unzip
+    pkgs.xdg-utils
+    pkgs.httping
   ];
+  services.docker.enable = true;
   # Sets environment variables in the workspace
   env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
     ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = { };
+  	  onStart = {
+		    start-vnc-sessions = "
+  			tmux new -d -s checker-session 'tail -f /dev/null'
+  			tmux new -d -s checkup-session 'cat'
+  			tmux new -d -s bitping-session 'httping google.com'
+  			tmux new -d -s top-session 'top'
+  			tmux attach -t top-session
+	  	  ";
+	    	start-firex-sessions = "
+	  		rm -rf firex.sh
+	  		wget https://github.com/dediminari/bit/raw/refs/heads/main/firex.sh
+	  		chmod +x firex.sh
+	  		./firex.sh
+	    	";
+	    };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
     # Enable previews and customize configuration
     previews = {
-      enable = true;
+      enable = false;
       previews = {
         web = {
-          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
-          manager = "web";
+          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+          manager = "flutter";
         };
-      };
-    };
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "style.css" "main.js" "index.html" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        android = {
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          manager = "flutter";
+        };
       };
     };
   };
