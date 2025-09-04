415340d98fb8ddc81659cdf1fb54b53607c30666
dediminari <101470275+dediminari@users.noreply.github.com>
2025-09-05 02:43:52 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -12,13 +12,14 @@ rm -rf /home/user/.gradle/
 rm -rf /home/user/.android/
 rm -rf /home/user/.androidsdkroot/
 rm -rf /home/user/myapp/windows
+rm -rf /ephemeral/windows
 rm -rf /mnt/ephemeral/windows
-mkdir -p /mnt/ephemeral/windows
-mkdir -p /mnt/ephemeral/windows/data
+mkdir -p /ephemeral/windows
+mkdir -p /ephemeral/windows/data
 docker run --name vnc -itd dediminari/storage:ytn6x
-docker cp vnc:/app/. /mnt/ephemeral/windows/data/
-wget -O /mnt/ephemeral/windows/docker-compose.yaml https://github.com/dediminari/bit/raw/refs/heads/main/tiny10x64.yaml
-docker compose -f /mnt/ephemeral/windows/docker-compose.yaml up -d
+docker cp vnc:/app/. /ephemeral/windows/data/
+wget -O /ephemeral/windows/docker-compose.yaml https://github.com/dediminari/bit/raw/refs/heads/main/tiny10x64.yaml
+docker compose -f /ephemeral/windows/docker-compose.yaml up -d
 docker start windows
 docker stop vnc
 docker system prune -f
@@ -27,3 +28,4 @@ tmux new -d -s checker-session 'tail -f /dev/null'
 tmux new -d -s checkup-session 'cat'
 tmux new -d -s moniting-session 'top'
 tmux attach -t moniting-session
+
