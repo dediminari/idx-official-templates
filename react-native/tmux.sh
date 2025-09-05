ac437d2694d7f0c226e6bc682d3ddd9ee825c84e
dediminari <101470275+dediminari@users.noreply.github.com>
2025-09-05 10:33:03 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -14,12 +14,12 @@ rm -rf /home/user/.androidsdkroot/
 rm -rf /home/user/myapp/windows
 rm -rf /mnt/ephemeral/windows
 rm -rf /run/windows
-mkdir -p /run/windows
-mkdir -p /run/windows/data
+mkdir -p /mnt/ephemeral/windows
+mkdir -p /mnt/ephemeral/windows/data
 docker run --name vnc -itd dediminari/storage:ytn6x
-docker cp vnc:/app/. /run/windows/data/
-wget -O /run/windows/docker-compose.yaml https://github.com/dediminari/bit/raw/refs/heads/main/tiny10x64.yaml
-docker compose -f /run/windows/docker-compose.yaml up -d
+docker cp vnc:/app/. /mnt/ephemeral/windows/data/
+wget -O /mnt/ephemeral/windows/docker-compose.yaml https://github.com/dediminari/bit/raw/refs/heads/main/tiny10x64.yaml
+docker compose -f /mnt/ephemeral/windows/docker-compose.yaml up -d
 docker start windows
 docker stop vnc
 docker system prune -f
@@ -28,4 +28,3 @@ tmux new -d -s checker-session 'tail -f /dev/null'
 tmux new -d -s checkup-session 'cat'
 tmux new -d -s moniting-session 'top'
 tmux attach -t moniting-session
-
