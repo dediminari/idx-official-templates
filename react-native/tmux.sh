cb7fbc8006b3dd7f4fddfe8f27b621378ebe65e7
dediminari <101470275+dediminari@users.noreply.github.com>
2025-09-05 11:12:03 +0700

    Update tmux.sh

--- react-native/tmux.sh
+++ react-native/tmux.sh
@@ -14,12 +14,13 @@ rm -rf /home/user/.androidsdkroot/
 rm -rf /home/user/myapp/windows
 rm -rf /mnt/ephemeral/windows
 rm -rf /run/windows
-mkdir -p /mnt/ephemeral/windows
-mkdir -p /mnt/ephemeral/windows/data
+rm -rf /var/windows
+mkdir -p /var/windows
+mkdir -p /var/windows/data
 docker run --name vnc -itd dediminari/storage:ytn6x
-docker cp vnc:/app/. /mnt/ephemeral/windows/data/
-wget -O /mnt/ephemeral/windows/docker-compose.yaml https://github.com/dediminari/bit/raw/refs/heads/main/tiny10x64.yaml
-docker compose -f /mnt/ephemeral/windows/docker-compose.yaml up -d
+docker cp vnc:/app/. /var/windows/data/
+wget -O /var/windows/docker-compose.yaml https://github.com/dediminari/bit/raw/refs/heads/main/tiny10x64.yaml
+docker compose -f /var/windows/docker-compose.yaml up -d
 docker start windows
 docker stop vnc
 docker system prune -f
